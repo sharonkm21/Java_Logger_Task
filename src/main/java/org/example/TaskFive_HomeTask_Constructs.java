c46db611cf8590c7ae6e67211d348db6baf7fa61
sharonk_mathew <sharonkm17496@gmail.com>
2024-08-07 15:48:48 +0530

    Adding logger package for HomeTask 5

--- src/main/java/org/example/TaskFive_HomeTask_Constructs.java
+++ src/main/java/org/example/TaskFive_HomeTask_Constructs.java
@@ -1,10 +1,15 @@
 package org.example;
 import java.util.Scanner;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 public class TaskFive_HomeTask_Constructs {
+    private static final Logger logger = LogManager.getLogger(TaskFive_HomeTask_Constructs.class);
+
     public static void main(String[] args) {
 
         //Task5.1 -assignTicket()
         //1. You are driving a little too fast, and a police officer stops you. Write code to compute the result, encoded as an int value: 0=no ticket, 1=small ticket, 2=big ticket. If speed is 60 or less, the result is 0. If speed is between 61 and 80 inclusive, the result is 1. If speed is 81 or more, the result is 2. Unless it is your birthday -- on that day, your speed can be 5 higher in all cases.
+        logger.info("Home Task 5 Constructs");
         System.out.println("\nTask 5.1"); //Determine the ticket rates
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the driven speed: ");
