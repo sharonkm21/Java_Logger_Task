f7243d266fd28b0ed0da0bfaa32a5b5379909b08
sharonk_mathew <sharonkm17496@gmail.com>
2024-08-07 17:26:14 +0530

    Adding logger package for GIT

--- src/main/java/org/example/TaskSix_HomeTask_LoopsNArrays.java
+++ src/main/java/org/example/TaskSix_HomeTask_LoopsNArrays.java
@@ -24,7 +24,7 @@ public class TaskSix_HomeTask_LoopsNArrays
 
         //Task6.3 -countClumps()
         //Say that a "clump" in an array is a series of 2 or more adjacent elements of the same value. Return the number of clumps in the given array.
-        logger.info("\nTask 6.3");
+        logger.info("\nTask 6.3 ");
         System.out.println(countClumps(new int[]{1, 2, 2, 3, 4, 4}));
         System.out.println(countClumps(new int[]{1, 2, 3, 4, 5}));
         logger.info("\nEnd of Task 6.3");
